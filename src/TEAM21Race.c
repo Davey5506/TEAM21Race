80f502df5272d144cfeae38b529b7411b629e5b2
David <kpdmmvi@gmail.com>
2025-12-04 13:15:17 -0500

    Updated code

--- src/TEAM21Race.c
+++ src/TEAM21Race.c
@@ -14,14 +14,14 @@
 #define STOP_SPEED 150
 
 // Ultrasonic Servo
-#define SENSOR_SERVO_CH TIM3->CCR3
+#define SENSOR_SERVO_CH TIM8->CCR3
 #define SERVO_LEFT 1280
 #define SERVO_CENTER 1500
 #define SERVO_RIGHT 1720
 
 enum PIN_VALUE sensors[4] = {PIN_ERROR, PIN_ERROR, PIN_ERROR, PIN_ERROR};
 volatile uint16_t speed[2] = {115, 110};
-volatile uint8_t stop_lines = 0;
+volatile uint8_t mode = 0;
 volatile uint16_t sensor = 0;
 volatile bool start = false;
 
@@ -47,15 +47,15 @@ uint32_t ultrasonic_measure(void){
 
 void sensor_left(void){
     SENSOR_SERVO_CH= SERVO_LEFT;
-    delay_ms(300);
+    delay_us(300);
 }
 void sensor_center(void){
     SENSOR_SERVO_CH= SERVO_CENTER;
-    delay_ms(300);
+    delay_us(300);
 }
 void sensor_right(void){
     SENSOR_SERVO_CH=SERVO_RIGHT;
-    delay_ms(300);
+    delay_us(300);
 }
 
 int direction(void){
@@ -97,6 +97,8 @@ void blank_drive(void){
     if(!sensors[0] && !sensors[1] && !sensors[2] && !sensors[3]){
         TIM3->CCR3 = SERVO_NEUTRAL_PULSE_WIDTH;
         TIM3->CCR4 = SERVO_NEUTRAL_PULSE_WIDTH;
+        TIM4->CR1 &= ~TIM_CR1_CEN; // Stop timer
+        mode = 1;
     }else if(sensors[0] && sensors[1] && sensors[2] && sensors[3]){
         move_forward();
     }else if(sensors[1] && sensors[2]){
@@ -124,15 +126,35 @@ void read_uv_sensors(void){
     if(sensors[1]) sensor += 100;
     if(sensors[2]) sensor += 10;
     if(sensors[3]) sensor += 1;
-
-    display_num(sensor, 0);
 }
 
 void EXTI15_10_IRQHandler(void){
     if(EXTI->PR & EXTI_PR_PR13){
         start = !start;
+        TIM4->CR1 |= TIM_CR1_CEN;
         EXTI->PR |= EXTI_PR_PR13; // Clear pending bit
     }
+}
+
+void PWM_Output_PC6_Init(void){
+    RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
+    TIM8->CR1 &= ~TIM_CR1_CEN;    
+    // 2. Set Timer Frequency (1MHz clock, 20ms period)
+    TIM8->PSC= (SYSTEM_FREQ / 1000000) - 1;
+    TIM8->ARR= 19999;
+
+    // 3. Configure TIM8 Channel 1 for PWM Mode 1
+    TIM8->CCMR1 &= ~(TIM_CCMR1_OC1M);
+    TIM8->CCMR1 |= (6 << TIM_CCMR1_OC1M_Pos);
+    TIM8->CCMR1 |= TIM_CCMR1_OC1PE;
+    
+    // 4. Enable Output and Main Output Enable
+    TIM8->CCER |= TIM_CCER_CC4E;
+    TIM8->BDTR |= TIM_BDTR_MOE;
+
+    TIM8->CNT = 0;
+    TIM8->EGR = TIM_EGR_UG;
+    TIM8->CR1 |= TIM_CR1_CEN;
 }
 
 int main(void){
@@ -150,7 +172,7 @@ int main(void){
     TIM3->CCR4 = SERVO_NEUTRAL_PULSE_WIDTH;
     TIM3->CR1 |= TIM_CR1_CEN;
 
-    init_gp_timer(TIM4, 1000, 30000, true);
+    init_gp_timer(TIM4, 1000, 0xFFFF, false);
     // Set up servos
     SERVO_t left_wheel = {
         .SERVO_PIN_PORT = GPIOC,
@@ -162,8 +184,16 @@ int main(void){
         .SERVO_PWM_PIN = 9,
         .SERVO_FEEDBACK_PIN = 16 // Does not exist, placeholder
     };
+    SERVO_t ultrasound_servo= {
+        .SERVO_PIN_PORT = GPIOC,
+        .SERVO_PWM_PIN = 6,
+        .SERVO_FEEDBACK_PIN = 16 // Does not exist, placeholder
+    };
+    init_servo(&ultrasound_servo);
     init_servo(&left_wheel);
     init_servo(&right_wheel);
+
+    PWM_Output_PC6_Init();
 
     // Setup PMOD C for sensors
     init_pmod(PMOD_C);
@@ -182,9 +212,12 @@ int main(void){
     NVIC_SetPriority(EXTI15_10_IRQn, 2);
 
     while(1){
+        display_num(TIM4->CNT/100, 1);
         read_uv_sensors();
-        if(start){
+        if(start && !mode){
             blank_drive();
+        }else if(start && mode){
+            direction();
         }else{
             TIM3->CCR3 = SERVO_NEUTRAL_PULSE_WIDTH;
             TIM3->CCR4 = SERVO_NEUTRAL_PULSE_WIDTH;
